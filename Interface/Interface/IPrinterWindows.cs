70df801a1ce88bc6b74f6fcb0bdc86d9feba2e86
bagus-ganteng <61874925+bagus-ganteng@users.noreply.github.com>
2020-05-14 17:00:34 +0700

    File

---
+++ Interface/Interface/IPrinterWindows.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Interface
+{
+   
+        public interface IPrinterWindows
+        {
+            void Show();
+            void Print();
+
+        }
+        public class LasetJet : IPrinterWindows
+        {
+            public void Show()
+            {
+                Console.WriteLine("LasetJet display dimension : 12*12");
+            }
+
+            public void Print()
+            {
+                Console.WriteLine("LasetJet printer printing ...");
+            }
+        }
+
+        public class Canon : IPrinterWindows
+        {
+            public void Show()
+            {
+                Console.WriteLine("Canon display dimension : 9.5*12");
+            }
+
+            public void Print()
+            {
+                Console.WriteLine("Canon printer printing ...");
+            }
+        }
+
+        public class Epson : IPrinterWindows
+        {
+            public void Show()
+            {
+                Console.WriteLine("Epson display dimension : 10*11");
+            }
+
+            public void Print()
+            {
+                Console.WriteLine("Epson printer printing ...");
+            }
+        }
+
+    
+}
---
+++ Interface/Interface/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace Interface
+
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            IPrinterWindows printer;
+
+            Console.WriteLine("Pilih Printer: ");
+            Console.WriteLine("1. Epson");
+            Console.WriteLine("2. Canon");
+            Console.WriteLine("3. LasetJet\n");
+
+            Console.WriteLine("Nomor Printer [1...3]:");
+            int nomorPrinter = Convert.ToInt32(Console.ReadLine());
+
+            if (nomorPrinter == 1)
+                printer = new Epson();
+            else if (nomorPrinter == 2)
+                printer = new Canon();
+            else
+                printer = new LasetJet();
+
+            printer.Show();
+            printer.Print();
+
+            Console.ReadKey();
+
+        }
+    }
+}
